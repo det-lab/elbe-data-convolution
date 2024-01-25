3e960fab8e609489415e8f04a337928ae1cd5e54
Arqu1100 <garrett@arquettes.net>
2024-01-25 12:33:01 -0700

    Added Gaussian function that is plotted Added a test for convolving the step function and Gaussian function

--- convolution.py
+++ convolution.py
@@ -3,17 +3,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def step_function(x):
-    if x < 0:
-        return 0
-    elif 0 <= x < 5:
+    if -6<= x < 6:
         return 1
     else:
         return 0
-
+    
 vectoredFunction = np.vectorize(step_function)
 
 x_values = np.linspace(-10, 10, 1000)
-
 y_value = vectoredFunction(x_values)
 
 plt.plot(x_values, y_value, label="Step Function")
@@ -22,3 +19,34 @@ plt.show()
 
 
 # Define a gaussion function and plot
+
+mean = 0
+standDiv = 1
+
+points = np.random.normal(mean, standDiv, 1000)
+xmin = min(points)
+xmax = max(points)
+
+plt.hist(points, bins=50, density=True, alpha=0.6, color='g')
+
+x = np.linspace(xmin, xmax, 100)
+
+gaussianFunc = np.exp(-(x - mean)**2 / (2 * standDiv**2)) / (standDiv * np.sqrt(2 * np.pi))
+
+plt.plot(x, gaussianFunc, 'k', linewidth=2)
+plt.show()
+print(xmin, xmax)
+
+
+
+# Convolve the Step function and the Gaussian Function and plot the convolved function
+
+convolveFunc = np.convolve(gaussianFunc, y_value, mode='full')
+
+plt.figure(figsize=(10,10))
+
+plt.plot()
+plt.stem(convolveFunc)
+plt.title('Convolution Result')
+
+plt.show()
