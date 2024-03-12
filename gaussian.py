Changed mass calculation for tof. Fixed extra zero in dfpath of gaussian.py Added print values of multipul values being calculated.

gaussian.py
@@ -17,27 +17,56 @@ print("reading file... " + file_path)
 
 neutronEnergy = pd.read_excel(file_path, usecols="E", header=0)
 
+#negNeutronEnergy = -neutronEnergy
+#
+#totalEngergy = pd.concat([negNeutronEnergy, neutronEnergy], ignore_index=True)
+#
+#totalEngergy = totalEngergy.sort_values(by="Lab Frame Energy")
+
 #function for simgma value in the gaussian function
 def sigma(neutronEnergy):
-    dfPath = 0.0349 #cm
-    fPath = 867.75 #cm
-    dTof = 0.2007  #*(10**(-7)) nanoseconds to centiseconds???
-    tof = np.sqrt(1.008665/(2*(neutronEnergy)))*fPath 
+    #mass = ((1.674 * 10 ** -27) * (1/(1.6*10**-19)) * (10**-6)) / ((2.98*10**8)**2)#mass energy in MeV
 
+    mass = 939.5654133 #/ (299792458) ** 2 # MeV per C^2 = mass
+
+    print("this is mass" , mass)
+    dfPath = 0.349 * 10 ** -2 #cm to m
+
+    fPath = 867.75 * 10 ** -2 #cm to m
+
+    dTof = 0.2007  * (10 ** (-9)) #nanoseconds to seconds
+    print("This is neutron energy",neutronEnergy)
+    # tof calculated with sqrt(mass/2Eng)*fpath
+    tof = np.sqrt((mass)/(2*(neutronEnergy)))*fPath / (299792458)
+    # sqrt((mev/c^2)/mev)*m = m/c = m/(m/s)
+    print("this is tof", tof)
     #returns a sigma value for an input of the neutron energy where tof is related to the energy as well
     return neutronEnergy * 2 * np.sqrt(((dfPath/fPath)**2) + (((dTof/tof))**2))
 
+#inprogress func
+def centerPointFunc():
+    x_values = np.linspace()
+
 #list of sigma values
+#sigmaList = sigma(neutronEnergy.iat[2,0])
 sigmaList = neutronEnergy.apply(sigma)
+print("This is sigma List", sigmaList)
 
-#mean of the x values or neutron energy
-mean = np.mean(neutronEnergy)
-
+print("sigma Pick at 2", sigmaList.iat[2,0])
+x_values = np.linspace(0.08, .11, 1000)
 #y axis as a gaussian function
-gaussianFunc = np.exp(-(neutronEnergy-mean)**2 / (2 * sigmaList**2)) / (sigmaList * np.sqrt(2 * np.pi))
+gaussianFunc = np.exp(-(x_values - neutronEnergy.iat[2,0])**2 / (2 * sigmaList.iat[2,0]**2)) / (sigmaList.iat[2,0] * np.sqrt(2 * np.pi))
+# e^(-MeV^2 / 2* MeV ^ 2) / (Mev * sqrt(2pi))
+print("this is gaussian values", gaussianFunc[50])
+
+
 
 #plots the gaussian function with the x axis being the neutron energy
-plt.plot(neutronEnergy, gaussianFunc, )
+#plt.plot(neutronEnergy, sigmaList)
+plt.plot(x_values, gaussianFunc)
 plt.ylabel("Gaussian Func")
 plt.xlabel("Neutron Energy (MeV)")
 plt.show()
+#print(totalEngergy.sort_values(by="Lab Frame Energy"))
+#print(negNeutronEnergy)
+#print(neutronEnergy)