5055be52ed65ff7ae50129d60c24c16d99a9d2ec
Arqu1100 <garrett@arquettes.net>
2024-04-05 16:28:28 -0700

    Few changes to the convolution of the gaussian, still a work in progress. doppler.pyhas two graphs plotting with two diffrent approaches.

--- doppler.py
+++ doppler.py
@@ -17,25 +17,60 @@ print("reading file... " + file_path)
 
 neutronEnergy = pd.read_excel(file_path, usecols="E", header=0)
 
+#Function to calculate doppler deviation
 def dopplerDiv(neutronEnergy):
-    boltsman = (1.380649 * 10**(-27)/(1.602*10**-19)) * 10**(-6)#Converted from Joules/Kkelvin to MeV/Kelvin
+    boltsman = (1.380649 * 10**(-23)/(1.602*10**-19))#Converted from Joules/kelvin to eV/Kelvin
+    boltsman = boltsman * 10**(-6) #convert eV/Kelvin to MeV/Kelvin
     temp = 293.15 #K
 
-    targetMass = 2.32 * 10 **(-23)
-    neutronMass = 1.674 * 10 **(-27)    
+    targetMass = 14.003 # Mass of N14 in Amu
+    neutronMass = 1.008 # Mass in Amu   
     massRatio = targetMass/neutronMass
 
     return np.sqrt(((2 * boltsman * temp)/massRatio) * neutronEnergy)
 
-dopplerDivList = neutronEnergy.apply(dopplerDiv)
+#applys the doppler function to each value of neutron energy returning a specific deviation
+dopplerDivFrame = neutronEnergy.apply(dopplerDiv)
 
-mean = np.mean(neutronEnergy)
+#converts data frames to list for more simple manipulation
+dopplerDivList = dopplerDivFrame["Lab Frame Energy"].tolist()
+neutronEnergyList = neutronEnergy["Lab Frame Energy"].tolist()
 
-gaussianFunc = np.exp(-(neutronEnergy - mean)**2 / (2 * dopplerDivList**2)) / (dopplerDivList * np.sqrt(2 * np.pi))
 
-print(dopplerDivList)
-
-plt.plot(neutronEnergy, gaussianFunc)
+#Function that plots points with a range of domain values
+for n, d in zip(neutronEnergyList,dopplerDivList):
+    x_values = np.linspace(n - 0.01, n +0.01, 10)
+    y_value = np.exp((-( x_values- n )**2 / (d**2))/(2)) / (d * np.sqrt(2 * np.pi))
+    print(y_value)
+    plt.plot(x_values,y_value)
 plt.ylabel("Doppler Broadening Func")
 plt.xlabel("Neutron Energy (MeV)")
 plt.show()
+
+
+#redfined vars to make following operation "readable"
+n = neutronEnergyList
+d = dopplerDivList
+
+#Creates a list of y values where the shift is zero
+y_value = [np.exp((-( 0 )**2 / (d**2))/(2)) / (d * np.sqrt(2 * np.pi)) for n, d in zip(neutronEnergyList,dopplerDivList)]
+    
+plt.plot(neutronEnergyList, y_value)
+plt.show()
+
+#x_values = np.linspace(neutronEnergyList[300]- 0.01, neutronEnergyList[300]+0.01, 200)
+
+#print(x_values)
+#mean = np.mean(neutronEnergy)
+
+#print(-(-x_values - neutronEnergyList[300])**2 / (2 * dopplerDivList**2))
+
+#gaussianFunc = np.exp((-(x_values- neutronEnergyList[300])**2 / (dopplerDivList[300]**2))/(2)) / (dopplerDivList[300] * np.sqrt(2 * np.pi))
+
+#print(dopplerDivList)
+#print(-(neutronEnergy)**2 / (2 * dopplerDivList**2))
+
+#plt.plot(x_values, y_value(dopplerDivList, neutronEnergyList))
+#plt.ylabel("Doppler Broadening Func")
+#plt.xlabel("Neutron Energy (MeV)")
+#plt.show()
--- gaussian.py
+++ gaussian.py
@@ -17,6 +17,12 @@ print("reading file... " + file_path)
 
 neutronEnergy = pd.read_excel(file_path, usecols="E", header=0)
 
+theoryValues = pd.read_excel(file_path,usecols="B", header=0)
+experemntValues = pd.read_excel(file_path,usecols="C", header=0)
+uncertainty = pd.read_excel(file_path,usecols="D", header=0)
+
+#print(theoryValues)
+
 #negNeutronEnergy = -neutronEnergy
 #
 #totalEngergy = pd.concat([negNeutronEnergy, neutronEnergy], ignore_index=True)
@@ -29,57 +35,100 @@ def sigma(neutronEnergy):
 
     mass = 939.5654133 #/ (299792458) ** 2 # MeV per C^2 = mass
 
-    print("this is mass" , mass)
+    #print("this is mass" , mass)
     dfPath = 0.349 * 10 ** -2 #cm to m
 
     fPath = 867.75 * 10 ** -2 #cm to m
 
     dTof = 0.2007  * (10 ** (-9)) #nanoseconds to seconds
-    print("This is neutron energy",neutronEnergy)
+    #print("This is neutron energy",neutronEnergy)
     # tof calculated with sqrt(mass/2Eng)*fpath
-    tof = np.sqrt((mass)/(2*(neutronEnergy)))*fPath / (299792458)
+    tof = np.sqrt((mass)/(2*(neutronEnergy)))*fPath / (299792458) #Speed of light
     # sqrt((mev/c^2)/mev)*m = m/c = m/(m/s)
-    print("this is tof", tof)
+    #print("this is tof", tof)
     #returns a sigma value for an input of the neutron energy where tof is related to the energy as well
     return neutronEnergy * 2 * np.sqrt(((dfPath/fPath)**2) + (((dTof/tof))**2))
 
 
 #list of sigma values
-#sigmaList = sigma(neutronEnergy.iat[2,0])
 sigmaDataFrame = neutronEnergy.apply(sigma)
 sigmaList = sigmaDataFrame["Lab Frame Energy"].tolist()
-print("This is sigma List", sigmaList)
+
 neutronEnergyList = neutronEnergy["Lab Frame Energy"].tolist()
-print("this is Neutron Energy List", neutronEnergyList)
 
-for i in range(len(sigmaList)):
-    pointer = sigmaList[i]
+theoryValuesList = theoryValues["Theory function"].tolist()
 
-    x_value = np.linspace(neutronEnergyList[i] - 0.01, neutronEnergyList[i] + 0.01, 200)
-    gaussianFuncProto = np.exp(((x_value - neutronEnergyList[i])**2)/(pointer**2) / (-2)) / (pointer * np.sqrt(2 * np.pi))
-    plt.plot(x_value, gaussianFuncProto)
-    print(x_value - neutronEnergyList[i])
-    print(2 * (pointer**2))
-    #print("gaussian Max Value at", i, np.exp(((x_value - neutronEnergyList[i])**2)/(pointer**2) / (-2)))
+
+#### Extreamly taxing code And Legacy Code ####
+
+#for i in range(len(sigmaList)):
+#    sigmaValue = sigmaList[i]
+#
+#    x_value = np.linspace(neutronEnergyList[i] - 0.01, neutronEnergyList[i] + 0.01, 200)
+#    gaussianFuncProto = np.exp(((x_value - neutronEnergyList[i])**2)/(sigmaValue**2) / (-2)) / (sigmaValue * np.sqrt(2 * np.pi))
+#    plt.plot(x_value, gaussianFuncProto)
+#    #print(x_value - neutronEnergyList[i])
+#    #print(2 * (pointer**2))
+#    #print("gaussian Max Value at", i, np.exp(((x_value - neutronEnergyList[i])**2)/(pointer**2) / (-2)))
+
+#### ####
+
+
+uncertaintyList = uncertainty["Uncertainty"].tolist()
+
+print(uncertaintyList[0])
+
+userNeutronEnergy = input("Select input from 0 - 3680\n")
+
+userNeutronEnergy = int(userNeutronEnergy)
+
+
+# rewrite so calculates guassian points at neutron energy values instead of around user inputs
+
+#x_value = np.linspace(neutronEnergyList[userNeutronEnergy] - 0.01, neutronEnergyList[userNeutronEnergy] + 0.01, 26)
+
+sigmaValue = sigmaList[userNeutronEnergy]
+print(neutronEnergyList[userNeutronEnergy])
+gaussianFuncProto = np.exp((((0)**2))/(sigmaValue**2) / (-2)) / (sigmaValue * np.sqrt(2 * np.pi)) * 1E-4
+
+experemntValuesList = experemntValues["Experimental data"].tolist()
+
+print(gaussianFuncProto)
+
+convovled = np.convolve(theoryValuesList , gaussianFuncProto , mode="same")
+
+print(convovled)
+
+plt.plot(neutronEnergyList, theoryValuesList, color = "orange")
+#plt.errorbar(neutronEnergyList, experemntValuesList, yerr = uncertaintyList, fmt="o",capsize = 5, color = "red")
+
+plt.plot(neutronEnergyList, convovled, color="blue")
+plt.xlabel("Neutron Energy MeV")
 
 plt.show()
 
 
-print("sigma Pick at 2", sigmaList[2])
-x_values = np.linspace(0.08, .11, 1000)
-#y axis as a gaussian function
-gaussianFunc = np.exp(-(x_values - neutronEnergy.iat[2,0])**2 / (2 * sigmaList.iat[2,0]**2)) / (sigmaList.iat[2,0] * np.sqrt(2 * np.pi))
-# expression of the gaussianFunc = e^(-MeV^2 / 2* MeV ^ 2) / (Mev * sqrt(2pi))
-print("this is gaussian values", gaussianFunc[50])
+#11.701
+#0.105
+#function for convovling resolution with theory values
 
+#Legacy Code
 
-
-#plots the gaussian function with the x axis being the neutron energy
-#plt.plot(neutronEnergy, sigmaList)
-plt.plot(x_values, gaussianFunc)
-plt.ylabel("Gaussian Func")
-plt.xlabel("Neutron Energy (MeV)")
-plt.show()
-#print(totalEngergy.sort_values(by="Lab Frame Energy"))
-#print(negNeutronEnergy)
-#print(neutronEnergy)
+#print("sigma Pick at 2", sigmaList)
+#x_values = np.linspace(0.08, .11, 1000)
+##y axis as a gaussian function
+#gaussianFunc = np.exp(-(neutronEnergy ** 2) / (2 * sigmaList**2)) / (sigmaList * np.sqrt(2 * np.pi))
+## expression of the gaussianFunc = e^(-MeV^2 / 2* MeV ^ 2) / (Mev * sqrt(2pi))
+##print("this is gaussian values", gaussianFunc)
+#
+#
+#
+##plots the gaussian function with the x axis being the neutron energy
+##plt.plot(neutronEnergy, sigmaList)
+#plt.plot(x_values, gaussianFunc)
+#plt.ylabel("Gaussian Func")
+#plt.xlabel("Neutron Energy (MeV)")
+#plt.show()
+##print(totalEngergy.sort_values(by="Lab Frame Energy"))
+##print(negNeutronEnergy)
+##print(neutronEnergy)
