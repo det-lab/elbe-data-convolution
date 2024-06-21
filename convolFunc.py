9706453bb71f77c173002d38e328527a5f879991
Arqu1100 <garrett@arquettes.net>
2024-06-20 23:52:04 -0700

    Rewrote Convolve function to perform same convolution instead of a full convolution

--- convolFunc.py
+++ convolFunc.py
@@ -1,28 +1,21 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from numba import jit
 
-#sameAxis = np.linspace(1,4,4)
-#
-#print(sameAxis)
+preList1 = np.linspace(5,10,12)  # Test numbers before but now can be scaled [1,2,3,4,5]
+preList2 = np.linspace(0,10,12)  # Test numbers before but now can be scaled [5,4,3,2,1]
+list1 = np.pad(preList1, len(preList2)-1, mode= "constant", constant_values=0)
 
-list1 = np.linspace(5,250,2000)  # Test numbers before but now can be scaled [1,2,3,4,5]
-list2 = np.linspace(0,300,2000)  # Test numbers before but now can be scaled [5,4,3,2,1]
-list1 = np.pad(list1, len(list2)-1, mode= "constant", constant_values=0)
 
 list1 = np.array(list1)
-list2 = np.array(list2)
+list2 = np.array(preList2)
 
-
+@jit(nopython = True)
 def convolv(list1, list2):
-    #flip first array
-    #list1Len = len(list1)
-    #list2Len = len(list2)
-    #if list2Len < list1Len:
-    #    return print("List1 is greater than list2")
     
     rArray = []
-    print(list1)
-    for i in range(len(list1)):
+    #Minus 1 in the inital value due to an even/odd relation for deviding the lengths by two 
+    for i in range(int(len(list1)/2)-1 ,len(list1)- ((len(preList2)-1)/2)):
         
         r = 0
 
@@ -32,62 +25,17 @@ def convolv(list1, list2):
                 break
             elif (j>= len(list2)) or ((i-j)>= len(list1)):
                 break
-            else:
+            elif (list2[j] != 0 and list1[i-j] != 0):
                 r += list2[j] * list1[i-j]
-
 
         rArray.append(r)
     
     return rArray
 
 # keep np.dot in mind
-                
-        #n = 1
-#
-        #x = list2[n] * list1[n- i] + list2[1] * list1[n-i]
-        #print(i)
-        #print(list2[n], list1[n - i],  list2[1],  list1[n-i])
-#
-        #print(x)
-
 
 x1_Value = np.linspace(-10,10,21) # n in y(n) convolve h(n)
 
-def yFunc(m):
-    yvalue = []
-    for i in range(len(x1_Value)):
-        if m[i] >= 0 and m[i] <2 or m[i] == 0 or  m[i] == 2:
-            yvalue.append(m[i] * 2)
-        else:
-            yvalue.append(0)
-
-    return yvalue
-        
-#h2_value = np.linspace(0,3,3)
-def hFunc(m):
-    hvalue = []
-    for i in range(len(x1_Value)):
-        if m[i] >= 0 and m[i] <2 or m[i] == 0 or  m[i] == 2:
-            hvalue.append(1)
-        else:
-            hvalue.append(0)
-
-    return hvalue
-
-def sumation():
-
-    newValueList = []
-
-    for i in range(x1_Value):
-
-        for j in range(len(x1_Value)):
-            
-            r = sum(hFunc(x1_Value[j]) * yFunc(x1_Value[i] - x1_Value[j]))
-        
-        print(i)
-        print(sum(hFunc(m) * yFunc(i-m)))
-
-    
 def cleanEdges(arry):
 
     startIdx = 0
@@ -108,21 +56,62 @@ def cleanEdges(arry):
 rawArry = convolv(list1,list2)
 cleanArry = cleanEdges(rawArry)
 
-print(len(cleanArry))
-
-convovleTestArry = np.convolve(list1,list2,mode="same")
+convovleTestArry = np.convolve(preList1,preList2,mode="same")
 cleanConTestArry = cleanEdges(convovleTestArry)
-print(len(cleanConTestArry))
+
+print("Length of Convolution testing array", len(cleanConTestArry),"\n", "Length of data array", len(cleanArry))
+
+print(cleanConTestArry,"\n", cleanArry)
+
 
 for i in range(len(cleanArry)):
-    if np.round(cleanArry[i],decimals=5) != np.round(cleanConTestArry[i],decimals=5):
+    if np.round(cleanArry[i],decimals=3) != np.round(cleanConTestArry[i],decimals=3):
         
-        print("### cleanArry and cleanCOnTestArry are not equal at", i,"###")
+        print("### cleanArry and cleanConTestArry are not equal at", i, "###")
+        print("### cleanArry =", cleanArry[i], "cleanConTestArry =", cleanConTestArry[i], "###","\n")
+        break
     elif len(cleanArry) != len(cleanConTestArry):
-        print("### Arrays are not the same dimentions ###")
+        print("### Arrays are not the same dimention ###")
+        print(len(cleanConTestArry),"\n", len(cleanArry))
+        break
 
 
 
+#def yFunc(m):
+#    yvalue = []
+#    for i in range(len(x1_Value)):
+#        if m[i] >= 0 and m[i] <2 or m[i] == 0 or  m[i] == 2:
+#            yvalue.append(m[i] * 2)
+#        else:
+#            yvalue.append(0)
+#
+#    return yvalue
+#        
+##h2_value = np.linspace(0,3,3)
+#def hFunc(m):
+#    hvalue = []
+#    for i in range(len(x1_Value)):
+#        if m[i] >= 0 and m[i] <2 or m[i] == 0 or  m[i] == 2:
+#            hvalue.append(1)
+#        else:
+#            hvalue.append(0)
+#
+#    return hvalue
+#
+#
+#def sumation():
+#
+#    newValueList = []
+#
+#    for i in range(x1_Value):
+#
+#        for j in range(len(x1_Value)):
+#            
+#            r = sum(hFunc(x1_Value[j]) * yFunc(x1_Value[i] - x1_Value[j]))
+#        
+#        print(i)
+#        print(sum(hFunc(m) * yFunc(i-m)))
+#
 
 
 #convolveFunc = np.convolve(yFunc(x1_Value),hFunc(x1_Value), mode="same")
